c88212291dafb4ab6968b1eaebb30b00a58b82f0
igotzedxd <74432210+igotzedxd@users.noreply.github.com>
2024-03-07 10:31:47 +0100

    xd

--- app/fetchData.js
+++ app/fetchData.js
@@ -3,7 +3,7 @@ const data = {};
 data.fetchData = async () => {
   try {
     const response = await fetch(
-      `https://dummyjson.com/products/search?q=phone`
+      `https://dummyjson.com/products/search?q=shirt`
     );
     const phoneData = await response.json();
     /*     console.log(phoneData);
--- app/renderProducts.js
+++ app/renderProducts.js
@@ -2,8 +2,11 @@ import data from "./fetchData.js";
 
 export const renderData = async () => {
   const phoneData = await data.fetchData();
+  const items = phoneData.products;
+  console.log(items);
   const output = document.querySelector(".output");
-  phoneData.forEach((phone) => {
-    output.innerHTML = `${phone.title} ${phone.price} ${phone.color} ${phone.company} ${phone.info}`;
+  items.forEach((item) => {
+    output.innerHTML += `<br>${item.title}<br> ${item.price}<br> ${item.description}<br><br>
+    <img src="${item.images[0]}" alt="${item.title}" width="200" height="200">`;
   });
 };
